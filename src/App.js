5ff81631a5109035329e7ffafeab4c33b705a2db
Bryan <bryannsanchez1234@gmail.com.com>
2023-09-20 13:39:17 -0700

    minor bugs with nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom'
 import Game from './components/game'
 import Nav from './components/nav'
 
+
 function App() {
   return (
-    <div className='app'>
-      <div className="app__games">
-        <Game />
-        <Game />
+    <Router>
+      <div className='app'>
+        <div className="app__games">
+          <Game />
+          <Game />
+        </div>
+
+
+
+
+
+
+        <Nav />
+
       </div>
-
-
-
-
-
-
-      {/* <Nav /> */}
-
-    </div>
+    </Router>
   );
 }
 
